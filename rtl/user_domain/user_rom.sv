2a620fbf2bb5feb2e83cd282aeede08958b8b461
Pigna099 <thomas.giochi@gmail.com>
2025-07-16 17:59:56 +0200

    Changes

---
+++ rtl/user_domain/user_rom.sv
@@ -0,0 +1,96 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Solderpad Hardware License, Version 0.51, see LICENSE for details.
+// SPDX-License-Identifier: SHL-0.51
+
+// gives us the `FF(...) macro making it easy to have properly defined flip-flops
+`include "common_cells/registers.svh"
+
+// simple ROM
+module user_rom #(
+  /// The OBI configuration for all ports.
+  parameter obi_pkg::obi_cfg_t           ObiCfg      = obi_pkg::ObiDefaultConfig,
+  /// The request struct.
+  parameter type                         obi_req_t   = logic,
+  /// The response struct.
+  parameter type                         obi_rsp_t   = logic
+) (
+  /// Clock
+  input  logic clk_i,
+  /// Active-low reset
+  input  logic rst_ni,
+
+  /// OBI request interface
+  input  obi_req_t obi_req_i,
+  /// OBI response interface
+  output obi_rsp_t obi_rsp_o
+);
+
+  localparam ADDR_SIZE = 8;
+
+  // Define some registers to hold the requests fields
+  logic req_d, req_q; // Request valid
+  logic we_d, we_q; // Write enable
+  logic [ObiCfg.AddrWidth-1:0] addr_d, addr_q; // Internal address of the word to read
+  logic [ObiCfg.IdWidth-1:0] id_d, id_q; // Id of the request, must be same for the response
+
+  // Signals used to create the response
+  logic [ObiCfg.DataWidth-1:0] rsp_data; // Data field of the obi response
+  logic rsp_err; // Error field of the obi response
+
+  // Wire the registers holding the request
+  assign req_d = obi_req_i.req;
+  assign id_d = obi_req_i.a.aid;
+  assign we_d = obi_req_i.a.we;
+  assign addr_d = obi_req_i.a.addr;
+  always_ff @(posedge (clk_i) or negedge (rst_ni)) begin
+    if (!rst_ni) begin
+      req_q <= '0;
+      id_q <= '0;
+      we_q <= '0;
+      addr_q <= '0;
+    end else begin
+      req_q <= req_d;
+      id_q <= id_d;
+      we_q <= we_d;
+      addr_q <= addr_d;
+    end
+  end
+
+  // Assign the response data
+  logic [ADDR_SIZE-1:0] word_addr;
+  always_comb begin
+    rsp_data = '0;
+    rsp_err  = '0;
+    word_addr = addr_q[ADDR_SIZE+1:2];
+
+    if(req_q) begin
+      if(~we_q) begin
+        case(word_addr)
+          // T.PIGNIANDC.DUHRA's ASIC = 54 2E 50 49 47 4E 49 41 4E 44 43 2E 44 55 68 52 41 27 73 20 41 53 49 43 in hex
+          3'b000: rsp_data= 32'h49502E54; 
+          3'b001: rsp_data= 32'h414E4749; 
+          3'b010: rsp_data= 32'h2E43444E; 
+          3'b011: rsp_data= 32'h27415248; 
+          3'b100: rsp_data= 32'h53412073;
+          3'b101: rsp_data= 32'h00004349;
+          3'b110: rsp_data= 32'h00000000;
+          3'b111: rsp_data= 32'h00000000;
+          default: rsp_data= 32'h0;
+        endcase
+      end else begin
+        rsp_err = '1;
+      end
+    end
+  end
+
+  // Wire the response
+  // A channel
+  assign obi_rsp_o.gnt = obi_req_i.req;
+  // R channel:
+  assign obi_rsp_o.rvalid = req_q;
+  assign obi_rsp_o.r.rdata = rsp_data;
+  assign obi_rsp_o.r.rid = id_q;
+  assign obi_rsp_o.r.err = rsp_err;
+  assign obi_rsp_o.r.r_optional = '0;
+
+endmodule
